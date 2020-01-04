92a306348eea3ade68679e4a2757b94b241dc988
AtomAnu <a.anuchitanukul@gmail.com>
2020-01-04 07:41:24 +0100

    Update playground.py

--- playground.py
+++ playground.py
@@ -2,8 +2,6 @@ from extract_data import format_data
 
 from models_parser import compare_regression_models, compare_classification_models
 
-import numpy as np
-
 training_file_name = 'epl-training.csv'
 test_file_name = 'epl-test.csv'
 training_data, test_data = format_data(training_file_name, test_file_name)
@@ -19,6 +17,7 @@ home_team_list = [col for col in training_data
                     for chosen_col in home_team_feature_name if col.startswith(chosen_col)]
 
 team_obj_list = []
+final_team_rating = []
 team_ratings = []
 for team in home_team_list:
     row_idx_list = []
@@ -33,5 +32,9 @@ for team in home_team_list:
     # compare_regression_models(team, group_8_data.iloc[row_idx_list].drop(columns=group_8_y), group_8_data[group_8_y].iloc[row_idx_list])
     final_prediction = compare_classification_models(team, group_8_data.iloc[row_idx_list].drop(columns=group_8_y), group_8_data[group_8_y].iloc[row_idx_list])
 
-    team_ratings.append(final_prediction)
+    for i in range(0, len(final_prediction))
+        final_team_rating[i] = sum(final_prediction[:i+1])/len(final_prediction[:i+1])
+
+    team_ratings.append(final_team_rating)
+
 print(team_ratings)
