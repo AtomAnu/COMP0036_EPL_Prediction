80c2e875eb8c7b379dbe43ad96acff545943ffa8
gbifvyihk <zlgan_1999@outlook.com>
2020-01-09 02:26:12 +0800

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -6,7 +6,6 @@ from keras.utils import to_categorical
 from keras import backend as K
 from keras import regularizers
 from sklearn import preprocessing
-from keras.callbacks import EarlyStopping
 import numpy as np
 import tensorflow as tf
 import h5py
@@ -16,27 +15,35 @@ import matplotlib.pyplot as plt
 from keras.callbacks import Callback
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
 
-class Metrics(Callback):
-    def on_train_begin(self, logs={}):
-        self.val_f1s = []
-        self.val_recalls = []
-        self.val_precisions = []
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
 
-    def on_epoch_end(self, epoch, logs={}):
-        val_predict = (np.asarray(self.model.predict(
-            self.validation_data[0]))).round()  # .model
-        val_targ = self.validation_data[1]  # .model
-        _val_f1 = f1_score(val_targ, val_predict, average='micro')
-        _val_recall = recall_score(val_targ, val_predict, average=None)
-        _val_precision = precision_score(val_targ, val_predict, average=None)
-        self.val_f1s.append(_val_f1)
-        self.val_recalls.append(_val_recall)
-        self.val_precisions.append(_val_precision)
-        #print("— val_f1: %f — val_precision: %f — val_recall: %f" %(_val_f1, _val_precision, _val_recall))
-        print("— val_f1: %f " % _val_f1)
-        return
+        Only computes a batch-wise average of recall.
 
-f1 = Metrics()
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 class Neural_Network:
     def __init__(self, input_size, output_size):
@@ -54,7 +61,7 @@ class Neural_Network:
         model = Sequential()
         model.add(Dense(32, input_dim=self.size_i, activation='relu'))
         model.add(Dense(self.size_o, activation='softmax'))
-        model.compile(loss='categorical_crossentropy',
+        model.compile(loss='mean_squared_error',
                       optimizer=self.optimizer, metrics=['accuracy', f1])
         model.summary()
         return model
